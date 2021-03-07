f899c385a10dac84855ccb43c7c13a76ca730825
yasu-wb <hasta.pronto0927@gmail.com>
2021-03-07 20:00:44 +0900

    新製品登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,23 @@
 class ItemsController < ApplicationController
   def index
+    
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :create
+    end
+  end
+
+  private
+  def item_params
+    params.permit(:incode, :name, :main_category, :sub_category)
   end
 end
