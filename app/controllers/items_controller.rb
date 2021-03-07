20758931a2d8f8cf707bf53fc651e73aca1c078d
yasu-wb <hasta.pronto0927@gmail.com>
2021-03-07 20:45:39 +0900

    path修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.permit(:incode, :name, :main_category, :sub_category)
+    params.require(:item).permit(:incode, :name, :main_category, :sub_category)
   end
 end
