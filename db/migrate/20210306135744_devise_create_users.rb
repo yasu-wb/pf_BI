2e9e5a4f190e4067382c037f32476bcc9229fe1f
yasu-wb <hasta.pronto0927@gmail.com>
2021-03-09 22:04:38 +0900

    ユーザー登録機能修正

--- db/migrate/20210306135744_devise_create_users.rb
+++ db/migrate/20210306135744_devise_create_users.rb
@@ -41,7 +41,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
     add_index :users, :number_id,            unique: true
-    add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2021_03_07_062721) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["number_id"], name: "index_users_on_number_id", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
