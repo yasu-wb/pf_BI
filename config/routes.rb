a24cf73bf212424f2305c267e599f1f787ea1eed
yasu-wb <hasta.pronto0927@gmail.com>
2021-04-05 22:08:20 +0900

    ゲストユーザーログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   root to:  'items#index'
   resources :items
   resources :lists do
