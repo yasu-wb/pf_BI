af7109750c9f02e546f758e37cdbef3ff64672bb
yasu-wb <hasta.pronto0927@gmail.com>
2021-04-18 19:26:25 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.16.0'
 set :application, 'pf_BI'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:yasu_wb/pf_BI.git'
+set :repo_url,  'git@github.com:yasu-wb/pf_BI.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
