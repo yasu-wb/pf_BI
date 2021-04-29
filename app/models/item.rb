f10a81b988d436a51837f029c33dc27543e5bc7a
yasu-wb <hasta.pronto0927@gmail.com>
2021-04-29 13:27:08 +0900

    INコードバリデーション
    6桁のバリデーションに変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,8 @@ class Item < ApplicationRecord
     validates :name
     validates :main_category_id, :sub_category_id, numericality: { other_than: 0, message: 'を選択してください' }
   end
-  validates :incode, numericality: { only_integer: true, message: 'は半角数字で入力してください' }
+      validates :incode, numericality: { only_integer: true, message: 'は半角数字で入力してください' }, length: { is: 6, message: '6桁で入力してください'}
+
   has_many :item_lists
   has_many :lists, through: :item_lists
   has_many :analyses
