582ec31ffdef77a59cb6794cdce7113668aef6cb
yasu-wb <hasta.pronto0927@gmail.com>
2021-05-04 13:15:50 +0900

    ルーティング修正、テストコード修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,19 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
+
   root to:  'items#index'
-  resources :items
-  resources :lists do
+
+  resources :items, except: :destroy
+
+  resources :lists, except: [:show, :edit, :update, :destroy] do
     collection do
       get :search
       get :result
     end
   end
-  resources :analyses do
+
+  resources :analyses, except: :destroy do
     collection do
       post :generatepdf
     end
